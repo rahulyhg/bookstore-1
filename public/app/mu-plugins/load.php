4c44e71245e4fb9483943b224ef17d626442e90a
Patrick Stewart <mrpastewart@yahoo.com>
2018-05-22 14:35:39 -0700

    other(refactor): adding a better loader

--- public/app/mu-plugins/load.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin Name: Mu-Plugins Loader
-Description: Simple loader in order to run the LaraPress framework core files.
-Author: Patrick Stewart
-Version: 1.0
-Author URI: http://www.larapress.io/
-*/
-require_once(WPMU_PLUGIN_DIR.'/larapress-framework/larapress.php');
